fc7b251175cf59e6e0f0041eb26a3aaae1bd5c6f
FABRICIO MALEK <fabricio.2912@aluno.pr.senac.br>
2025-09-24 08:58:03 -0300

    Crian flags no modal, atribuindo a renderização na constante para exibir as flags

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -3,9 +3,15 @@ import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-nati
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 import { Modalize } from "react-native-modalize";
 import { Input } from "../components/input";
+import { themas } from "../global/themes";
+import { Flag } from "../components/Flag";
 
 export const AuthContextList: any = createContext({});
 
+const flags = [
+    { caption: 'Urgente', color: themas.colors.red },
+    { caption: 'Opcional', color: themas.colors.blueLight }
+]
 export const AuthProviderList = (props: any): any => {
 
     const modalizeRef = useRef<Modalize>(null);
@@ -16,6 +22,20 @@ export const AuthProviderList = (props: any): any => {
     useEffect(() => {
         onOpen()
     }, [])
+
+    const _renderFlags=()=>{
+        return(
+            flags.map((item, index) => (
+                <TouchableOpacity>
+                    <Flag
+                        caption={item.caption}
+                        color={item.color}
+                    />
+                </TouchableOpacity>
+            ))
+        )
+    }
+
     const _container = () => {
         return (
             <View style={styles.container}>
@@ -54,8 +74,8 @@ export const AuthProviderList = (props: any): any => {
                 </View>
                 <View style={styles.containerFlag}>
                     <Text style={styles.label}>Flags:</Text>
-                    <View style={{}}>
-
+                    <View style={styles.rowFlags}>
+                        {_renderFlags()}
                     </View>
                 </View>
             </View>
@@ -82,16 +102,16 @@ export const styles = StyleSheet.create({
         width: '100%'
     },
     header: {
-        width:'100%',
+        width: '100%',
         height: 40,
-        paddingHorizontal:40,
+        paddingHorizontal: 40,
         flexDirection: 'row',
         marginTop: 20,
-        justifyContent:'space-between',
+        justifyContent: 'space-between',
         alignItems: 'center'
     },
     containerFlag: {
-        width:'100%',
+        width: '100%',
         padding: 10
     },
     label: {
@@ -102,9 +122,14 @@ export const styles = StyleSheet.create({
         width: '100%',
         paddingHorizontal: 20
     },
-    title:{
+    title: {
         fontSize: 20,
         fontWeight: 'bold'
+    },
+    rowFlags:{
+        flexDirection: 'row',
+        gap: 10,
+        marginTop:10,
     }
 
 })
